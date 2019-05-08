ed3a8292ccc0e1472d589756b463e52832ddb61a
Steve Godin <steve.god@gmail.com>
2019-05-08 14:07:53 -0700

    Add demo for Question Details

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<h1>Hi, Universe!</h1>, document.getElementById("root"));
+function QuestionDetails() {
+  return (
+    <div>
+      <h2>What is your favourite color?</h2>
+      <p>
+        Red, green, blue, magenta, etc. <br />
+        By Bridge Troll
+      </p>
+      <p>
+        <small>Seen 10 time(s)</small> – <small>Created 10 days ago</small>
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<QuestionDetails />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
