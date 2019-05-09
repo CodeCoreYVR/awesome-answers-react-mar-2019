e5742dc11bcc71b23ba6fdcb1ed422a3aefde4bd
Steve Godin <steve.god@gmail.com>
2019-05-09 13:48:09 -0700

    Add demo for Question Show goes to Class

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { AnswerDetails } from "./AnswerDetails";
 import { AnswerList } from "./AnswerList";
 import { QuestionDetails } from "./QuestionDetails";
@@ -8,18 +8,20 @@ import questionData from "../questionData";
 // To structure our application, we will create components
 // that simulate the pages of web application. These are meant
 // to replace the various pages rendered by the routes of our rails server.
-export function QuestionShowPage() {
-  return (
-    <main className="Page">
-      <QuestionDetails
-        title="What's your favourite colour?"
-        body="Red, green, blue, seafoam green, turquoise, etc."
-        author={{ full_name: "Bridge Troll" }}
-        view_count={100}
-        created_at={new Date().toLocaleString()}
-      />
-      <h2>Answers</h2>
-      <AnswerList answers={questionData.answers} />
-    </main>
-  );
+export class QuestionShowPage extends Component {
+  render() {
+    return (
+      <main className="Page">
+        <QuestionDetails
+          title="What's your favourite colour?"
+          body="Red, green, blue, seafoam green, turquoise, etc."
+          author={{ full_name: "Bridge Troll" }}
+          view_count={100}
+          created_at={new Date().toLocaleString()}
+        />
+        <h2>Answers</h2>
+        <AnswerList answers={questionData.answers} />
+      </main>
+    );
+  }
 }
