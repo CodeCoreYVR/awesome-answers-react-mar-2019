bd5503ea9ac9d4f9928a4f8fc4cbbc49aa0d40c2
Steve Godin <steve.god@gmail.com>
2019-05-09 14:05:17 -0700

    Add solution to Question Index goes to Class

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import data from "../questionsData";
 
-export function QuestionIndexPage(props) {
-  return (
-    <main className="Page">
-      <h2>Questions</h2>
-      <ul
-        style={{
-          listStyle: "none",
-          paddingLeft: 0
-        }}
-      >
-        {data.map(question => (
-          <li key={question.id}>
-            <a href="">{question.title}</a>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+export class QuestionIndexPage extends Component {
+  render() {
+    return (
+      <main className="Page">
+        <h2>Questions</h2>
+        <ul
+          style={{
+            listStyle: "none",
+            paddingLeft: 0
+          }}
+        >
+          {data.map(question => (
+            <li key={question.id}>
+              <a href="">{question.title}</a>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
 }
