030ad6945d372475e69558d0ff6cc9ad4ac5eca7
Steve Godin <steve.god@gmail.com>
2019-05-09 15:02:25 -0700

    Add demo for Delete Question from Index

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import data from "../questionsData";
 
 export class QuestionIndexPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: data
+    };
+  }
+
+  deleteQuestion(id) {
+    // To change state in a React component, you must use the
+    // `setState()` method on `this`. It takes an object that gets
+    // merged in the current state at React's convenience.
+    // The properties in `setState()` replace the same name properties
+    // in the current state.
+    // This happens asynchronously and will eventually trigger an update
+    // to the DOM if there's any change.
+    this.setState({
+      questions: this.state.questions.filter(q => q.id !== id)
+    });
+  }
+
   render() {
     return (
       <main className="Page">
@@ -12,9 +33,13 @@ export class QuestionIndexPage extends Component {
             paddingLeft: 0
           }}
         >
-          {data.map(question => (
+          {this.state.questions.map(question => (
             <li key={question.id}>
-              <a href="">{question.title}</a>
+              <a href="">{question.title}</a> <br />
+              {/* buttonNode.addEventListener("click", event => ...) */}
+              <button onClick={() => this.deleteQuestion(question.id)}>
+                Delete
+              </button>
             </li>
           ))}
         </ul>
