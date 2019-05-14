346c4c46e8da0a52f7a9df4ae4846e1736cecced
Steve Godin <steve.god@gmail.com>
2019-05-14 15:59:28 -0700

    Re-factor QuestionNewPage with QuestionForm

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FormErrors } from "./FormErrors";
+
+export const QuestionForm = props => {
+  const { errors = [] } = props;
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formNode = event.currentTarget;
+    const formData = new FormData(formNode);
+
+    if (typeof props.onSubmit === "function") {
+      props.onSubmit({
+        title: formData.get("title"),
+        body: formData.get("body")
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title</label> <br />
+        <FormErrors forField="title" errors={errors} />
+        <input name="title" id="title" />
+      </div>
+
+      <div>
+        <label htmlFor="body">Body</label> <br />
+        <FormErrors forField="body" errors={errors} />
+        <textarea name="body" id="body" cols="60" rows="4" />
+      </div>
+
+      <div>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  );
+};
--- src/components/QuestionNewPage.js
+++ src/components/QuestionNewPage.js
@@ -2,21 +2,15 @@ import React, { Component } from "react";
 import { Question } from "../api/question";
 import { Session } from "../api/session";
 import { FormErrors } from "./FormErrors";
+import { QuestionForm } from "./QuestionForm";
 
 export class QuestionNewPage extends Component {
   state = {
     errors: []
   };
 
-  createQuestion(event) {
-    event.preventDefault();
-    const { currentTarget } = event;
-    const formData = new FormData(currentTarget);
-
-    Question.create({
-      title: formData.get("title"),
-      body: formData.get("body")
-    }).then(data => {
+  createQuestion(params) {
+    Question.create(params).then(data => {
       if (!data.errors) {
         // The `history` prop is provide by the <Route>
         // component. It has a bunch of methods to manipulate
@@ -24,7 +18,6 @@ export class QuestionNewPage extends Component {
         // page in our app.
         this.props.history.push(`/questions/${data.id}`);
       } else {
-        console.log(data.errors);
         this.setState({
           errors: data.errors
         });
@@ -39,23 +32,10 @@ export class QuestionNewPage extends Component {
 
         {/* <FormErrors errors={this.state.errors} /> */}
 
-        <form onSubmit={event => this.createQuestion(event)}>
-          <div>
-            <label htmlFor="title">Title</label> <br />
-            <FormErrors forField="title" errors={this.state.errors} />
-            <input name="title" id="title" />
-          </div>
-
-          <div>
-            <label htmlFor="body">Body</label> <br />
-            <FormErrors forField="body" errors={this.state.errors} />
-            <textarea name="body" id="body" cols="60" rows="4" />
-          </div>
-
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
+        <QuestionForm
+          errors={this.state.errors}
+          onSubmit={params => this.createQuestion(params)}
+        />
       </main>
     );
   }
