ed872bc1763e2165fc422a6cf130a08f7854ea97
Steve Godin <steve.god@gmail.com>
2019-05-14 15:33:11 -0700

    Create FormErrors component to handle QuestionNewPage validation

---
+++ src/components/FormErrors.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const FormErrors = props => {
+  const { errors = [], forField } = props;
+  // <FormErrors forField="body" errors={erros} />
+
+  let filteredErrors = errors;
+  if (forField) {
+    filteredErrors = errors.filter(error => error.field === forField);
+  }
+
+  return (
+    <ul className="FormErrors">
+      {filteredErrors.map((error, index) => (
+        <li key={index}>
+          {forField ? error.message : `${error.field} ${error.message}`}
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/components/QuestionNewPage.js
+++ src/components/QuestionNewPage.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import { Question } from '../api/question';
-import { Session } from '../api/session';
+import React, { Component } from "react";
+import { Question } from "../api/question";
+import { Session } from "../api/session";
+import { FormErrors } from "./FormErrors";
 
 export class QuestionNewPage extends Component {
+  state = {
+    errors: []
+  };
+
   createQuestion(event) {
     event.preventDefault();
     const { currentTarget } = event;
     const formData = new FormData(currentTarget);
 
     Question.create({
-      title: formData.get('title'),
-      body: formData.get('body'),
-    })
-    .then((data) => {
+      title: formData.get("title"),
+      body: formData.get("body")
+    }).then(data => {
       if (!data.errors) {
         // The `history` prop is provide by the <Route>
         // component. It has a bunch of methods to manipulate
         // browser. You can use `push` to direct a user to any
         // page in our app.
         this.props.history.push(`/questions/${data.id}`);
+      } else {
+        console.log(data.errors);
+        this.setState({
+          errors: data.errors
+        });
       }
     });
   }
@@ -27,14 +36,19 @@ export class QuestionNewPage extends Component {
     return (
       <main className="Page">
         <h1>Ask a Question</h1>
-        <form onSubmit={(event) => this.createQuestion(event)}>
+
+        {/* <FormErrors errors={this.state.errors} /> */}
+
+        <form onSubmit={event => this.createQuestion(event)}>
           <div>
             <label htmlFor="title">Title</label> <br />
+            <FormErrors forField="title" errors={this.state.errors} />
             <input name="title" id="title" />
           </div>
 
           <div>
             <label htmlFor="body">Body</label> <br />
+            <FormErrors forField="body" errors={this.state.errors} />
             <textarea name="body" id="body" cols="60" rows="4" />
           </div>
 
